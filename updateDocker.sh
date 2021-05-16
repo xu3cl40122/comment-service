35ac30211b867b574bde4de812996c452b667963
tom lee <xu3cl40122@gmail.com>
2021-05-16 11:12:08 +0800

    Fix sh

--- updateDocker.sh
+++ updateDocker.sh
@@ -1,3 +1,3 @@
-docker-compose -f docker-compose.prod.yml pull
-docker-compose -f docker-compose.prod.yml up -d --remove-orphans
+docker-compose -f ./docker-compose.prod.yml pull
+docker-compose -f ./docker-compose.prod.yml up -d --remove-orphans
 docker image prune -f
